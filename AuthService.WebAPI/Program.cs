70c7cfe592bed8227281eb7ab91d47ab10f17c93
ertu426 <erxat124@gmail.com>
2023-06-10 01:31:28 +0800

    feat: add cors config

--- AuthService.WebAPI/Program.cs
+++ AuthService.WebAPI/Program.cs
@@ -4,16 +4,26 @@ using AuthService.WebAPI.Configure.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var configuration = builder.Configuration;
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// 获取 配置文件中 secretKey 的值
-IConfiguration jwtSettings = builder.Configuration.GetSection("JwtSettings");
+#region AddJwtAuthentication
+
+IConfiguration jwtSettings = configuration.GetSection("JwtSettings");
 string secretKey = jwtSettings.GetSection("SecretKey").Value!;
 builder.Services.AddJwtAuthentication(secretKey);
+
+#endregion
+
+#region AddCors
+
+builder.Services.AddAllowCors(configuration);
+
+#endregion
 
 var app = builder.Build();
 
@@ -22,11 +32,18 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseCors("AllowAllCors");
 }
+else
+{
+    app.UseCors();
+}
+
+
 
 app.UseHttpsRedirection();
 
-app.UseJwtApplication();
+app.UseApplication();
 
 app.MapControllers();
 
