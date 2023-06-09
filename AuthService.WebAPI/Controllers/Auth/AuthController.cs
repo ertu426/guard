90f210d60dc5441bbdd13a2e5bcab32e218c80c5
onenine <41768270+ertu426@users.noreply.github.com>
2023-06-09 14:22:58 +0800

    feat: add login (#2)
    * feat: add package JwtBearer
    
    * feat: add jwt Authentication
    
    * feat: add login action

---
+++ AuthService.WebAPI/Controllers/Auth/AuthController.cs
@@ -0,0 +1,24 @@
+﻿using AuthService.WebAPI.Utils.Jwt;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthService.WebAPI.Controllers.Auth;
+
+[Route("[controller]")]
+[ApiController]
+public class AuthController: ControllerBase
+{
+    private readonly JwtHelper _jwtHelper;
+
+    public AuthController(IConfiguration configuration)
+    {
+        string? secretKey = configuration.GetSection("JwtSettings:SecretKey").Value;
+        _jwtHelper = new JwtHelper(secretKey);
+    }
+
+    [HttpPost, Route("[action]")]
+    public IActionResult Login([FromBody] LoginRequest request)
+    {
+        string token = _jwtHelper.GenerateToken(request.Username);
+        return Ok(new { code = "200", data = new { token }, message = "success" });
+    }
+}
