ba8ef7f2b200389993e95bb2b5eb93dc92847b2b
onenine <41768270+ertu426@users.noreply.github.com>
2023-06-10 13:37:10 +0800

    Revert "feat: add register (#3)"
    This reverts commit 1e478a2de8125c5a2086f3fbdd7e5f6c25cbcfe5.

--- AuthService.WebAPI/Models/UserModels.cs
+++
@@ -1,8 +0,0 @@
-﻿using AuthService.WebAPI.Models.Auth;
-
-namespace AuthService.WebAPI.Models;
-
-public static class UserModels
-{
-    public static List<User> Users { get; set; } = new List<User>();
-}
--- AuthService.WebAPI/Program.cs
+++ AuthService.WebAPI/Program.cs
@@ -1,29 +1,11 @@
-using AuthService.WebAPI.Configure;
-using AuthService.WebAPI.Configure.Services;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var configuration = builder.Configuration;
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-#region AddJwtAuthentication
-
-IConfiguration jwtSettings = configuration.GetSection("JwtSettings");
-string secretKey = jwtSettings.GetSection("SecretKey").Value!;
-builder.Services.AddJwtAuthentication(secretKey);
-
-#endregion
-
-#region AddCors
-
-builder.Services.AddAllowCors(configuration);
-
-#endregion
 
 var app = builder.Build();
 
@@ -32,18 +14,11 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
-    app.UseCors("AllowAllCors");
 }
-else
-{
-    app.UseCors();
-}
-
-
 
 app.UseHttpsRedirection();
 
-app.UseApplication();
+app.UseAuthorization();
 
 app.MapControllers();
 
